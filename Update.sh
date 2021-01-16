699679cd36683facdb0992db2adf17c6aca06d37
Moexin <moexin@qq.com>
2021-01-16 20:18:30 +0800

    Update

---
+++ Update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo '正在停止后端'
+systemctl stop TCat
+echo '正在删除后端'
+rm -rf /root/TCat
+echo '正在下载后端'
+while true;do wget -P /root/TCat -T 15 -c -q https://cdn.jsdelivr.net/gh/TCatCloud/Server/TCat && break;done
+while true;do wget -P /root/TCat -T 15 -c -q https://cdn.jsdelivr.net/gh/TCatCloud/Server/TCat.zip && break;done
+unzip /root/TCat/TCat.zip -d /root/TCat > /dev/null
+chmod +x /root/TCat/*
+echo '正在启动后端'
+systemctl start TCat
+echo '正在停止后端'
+systemctl stop Hash
+echo '正在删除后端'
+rm -rf /root/Hash
+echo '正在下载后端'
+while true;do wget -P /root/Hash -T 15 -c -q https://cdn.jsdelivr.net/gh/HashVPN/Server/Hash && break;done
+while true;do wget -P /root/Hash -T 15 -c -q https://cdn.jsdelivr.net/gh/HashVPN/Server/Hash.zip && break;done
+unzip /root/Hash/Hash.zip -d /root/Hash > /dev/null
+chmod +x /root/Hash/*
+echo '正在启动后端'
+systemctl start Hash
