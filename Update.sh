d6850ca0f9ab3acce8127653902c437847943205
Moexin <moexin@qq.com>
2021-03-01 18:21:17 +0800

    Update

--- Update.sh
+++ Update.sh
@@ -4,8 +4,8 @@ systemctl stop TCat
 echo '正在删除后端'
 rm -rf /root/TCat
 echo '正在下载后端'
-wget -P /root/TCat -q https://cdn.jsdelivr.net/gh/TCatCloud/Server/TCat
-wget -P /root/TCat -q https://cdn.jsdelivr.net/gh/TCatCloud/Server/TCat.zip
+wget -P /root/TCat -T 15 -t 30 -c -nv https://raw.githubusercontent.com/TCatCloud/Server/master/TCat
+wget -P /root/TCat -T 15 -t 30 -c -nv https://raw.githubusercontent.com/TCatCloud/Server/master/TCat.zip
 unzip /root/TCat/TCat.zip -d /root/TCat > /dev/null
 chmod +x /root/TCat/*
 echo '正在启动后端'
@@ -15,8 +15,8 @@ systemctl stop Hash
 echo '正在删除后端'
 rm -rf /root/Hash
 echo '正在下载后端'
-wget -P /root/Hash -q https://cdn.jsdelivr.net/gh/HashVPN/Server/Hash
-wget -P /root/Hash -q https://cdn.jsdelivr.net/gh/HashVPN/Server/Hash.zip
+wget -P /root/Hash -T 15 -t 30 -c -nv https://raw.githubusercontent.com/HashVPN/Server/master/Hash
+wget -P /root/Hash -T 15 -t 30 -c -nv https://raw.githubusercontent.com/HashVPN/Server/master/Hash.zip
 unzip /root/Hash/Hash.zip -d /root/Hash > /dev/null
 chmod +x /root/Hash/*
 echo '正在启动后端'
