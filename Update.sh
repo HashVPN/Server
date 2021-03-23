8ce50c9609d1c0bfaf6c6976de6c21ebcf7ac090
Moexin <moexin@qq.com>
2021-03-23 14:01:39 +0800

    Update

--- Update.sh
+++ Update.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 systemctl stop TCat
-systemctl stop Hash
 rm -rf /root/TCat
-rm -rf /root/Hash
-wget -P /root/TCat -T 15 -t 30 -c -q https://raw.githubusercontent.com/TCatCloud/Server/master/TCat
-wget -P /root/Hash -T 15 -t 30 -c -q https://raw.githubusercontent.com/HashVPN/Server/master/Hash
-wget -P /root/TCat -T 15 -t 30 -c -q https://raw.githubusercontent.com/TCatCloud/Server/master/TCat.zip
-wget -P /root/Hash -T 15 -t 30 -c -q https://raw.githubusercontent.com/HashVPN/Server/master/Hash.zip
-unzip /root/TCat/TCat.zip -d /root/TCat > /dev/null
-unzip /root/Hash/Hash.zip -d /root/Hash > /dev/null
+wget -P /root/TCat -T 15 -t 30 -c --retry-connrefused https://raw.githubusercontent.com/TCatCloud/Server/master/TCat
+wget -P /root/TCat -T 15 -t 30 -c --retry-connrefused https://raw.githubusercontent.com/TCatCloud/Server/master/TCat.zip
+unzip /root/TCat/TCat.zip -d /root/TCat
+rm -f /root/TCat/TCat.zip
 chmod +x /root/TCat/*
-chmod +x /root/Hash/*
 systemctl start TCat
+systemctl stop Hash
+rm -rf /root/Hash
+wget -P /root/Hash -T 15 -t 30 -c --retry-connrefused https://raw.githubusercontent.com/HashVPN/Server/master/Hash
+wget -P /root/Hash -T 15 -t 30 -c --retry-connrefused https://raw.githubusercontent.com/HashVPN/Server/master/Hash.zip
+unzip /root/Hash/Hash.zip -d /root/Hash
+rm -f /root/Hash/Hash.zip
+chmod +x /root/Hash/*
 systemctl start Hash
-sleep 5
-systemctl status TCat
-systemctl status Hash
+wget -P /root -T 15 -t 30 -c --retry-connrefused https://raw.githubusercontent.com/TCatCloud/Server/master/System-Optimize.sh
+source /root/System-Optimize.sh
+install_bbr
+rm -f /root/System-Optimize.sh
